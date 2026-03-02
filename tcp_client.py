beb5a67f88fa25237d144dbbc290808f2b16fb16
JialaChang <jiala20061218@gmail.com>
2026-03-02 23:19:55 +0800

    將不同功能分門別類，不再像以前全混一起

--- tcp_client.py
+++ tcp_client.py
@@ -5,38 +5,110 @@ import platform
 import os
 
 
-class Network:
-
-    def __init__(self):
-
+""" --- 網路通訊 --- """
+class NetworkClient:
+    
+    def __init__(self, on_receive_callback):
         self.s = None
         self.is_connected = False
+        
+        # 藉由 callback 的方法調用 AppWindow 的 handle_incoming, 避免模組間的強耦合
+        self.on_receive = on_receive_callback   
+
+
+    def establish_connect(self, ip, port):
+        """建立連線"""
+
+        try:
+            # AF_INET : IPv4 協定 ; SOCK_STREAM : TCP 協定
+            self.s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)  
+            self.s.connect((ip, int(port)))
+            self.is_connected = True
+
+            # 用threading建立多執行緒
+            # daemon=True -> 主執行緒被關閉時, 副執行緒也同樣會被關閉 
+            t = threading.Thread(target=self.receive_data, daemon=True) 
+            t.start()
+
+            return True, ""
+            
+        except Exception as e:
+            self.s.close()
+            return False, str(e)
+
+
+    def stop_connect(self):
+        """中斷連線"""
+
+        self.is_connected = False
+        if self.s:
+            try:
+                self.s.shutdown(socket.SHUT_RDWR)
+                self.s.close()
+            except:
+                pass
+        
+
+    def receive_data(self):
+        """接收資料"""
+
+        while self.is_connected:
+            try:      
+                data = self.s.recv(1024)    # 1024 bytes
+                if not data:
+                    # 斷線會在最後傳空的封包
+                    self.on_receive(None)
+                    break
+
+                # 將資料傳回 AppWindow 內
+                self.on_receive(data)
+
+            except Exception as e:
+                if self.is_connected:
+                    self.on_receive(e)
+                break
+
+        self.is_connected = False
+
+    
+""" --- 資料處理 --- """
+class DataProcess:
+
+    def format_output(self, data , data_format):
+        """資料輸出型態"""
+
+        if data_format == "Text(UTF-8)":
+            return (f">> {data.decode('utf-8', errors='replace')}")
+        elif data_format == "Hex":
+            # hex(' ') 的作用是在每個 Byte 之間插空格
+            return (f">> {data.hex(' ').upper()}") 
+        elif data_format == "Binary":
+            # for 跑 bytes (self.data) 會把每個 byte 變成 0~255 的整數
+            # '08b' : b -> 二進位, 8 -> 讓字串是八個字元, 0 -> 如果有缺補零
+            return (f">> {' '.join(format(byte, '08b') for byte in data)}")
+    
+
+""" --- 視窗介面 --- """
+class AppWindow:
+
+    def __init__(self):
+        self.processor = DataProcess()
+        
+        # 將 handle_incoming 這個函式透過 on_receive_callback 交給 NetworkClient
+        self.network = NetworkClient(on_receive_callback=self.handle_incoming)  
+
+        self.setup_gui()
+
         self.target_ip = ""
         self.target_port = ""
         self.data_format = ""
-
-        self.setup_gui()
 
 
     def setup_gui(self):
         """建立視窗 UI """
 
         dpg.create_context()
-
-        # 文字設定
-        with dpg.font_registry():
-            font_path = ""
-            if platform.system() == "Windows":
-                font_path = "C:/Windows/Fonts/msjh.ttc"  # 微軟正黑體
-            elif platform.system() == "Darwin":
-                font_path = "/System/Library/Fonts/PingFang.ttc" # Mac 蘋方體
-
-            if font_path and os.path.exists(font_path):
-                with dpg.font(font_path, 18) as default_font:
-                    dpg.add_font_range_hint(dpg.mvFontRangeHint_Default)
-                    dpg.add_font_range_hint(dpg.mvFontRangeHint_Chinese_Full)
-
-                dpg.bind_font(default_font)
+        self.load_font()
 
         # 建立主視窗 UI, DearPyGui 底層是 C++ 因此不用 (用self) 儲存 Python 物件
         with dpg.window(tag="primary_window"):
@@ -57,7 +129,7 @@ class Network:
                 )
 
                 dpg.add_text(" " * 5) 
-                dpg.add_button(label="Save", tag="btn_setip", callback=self.save_setting) # callback 會傳入 sender, app_data, user_data 三個參數
+                dpg.add_button(label="Save", tag="btn_setip", callback=self.save_setting) 
 
                 dpg.add_text(" " * 20)
                 dpg.add_button(label="Connect", tag="btn_connect", callback=self.toggle_connection)
@@ -77,6 +149,24 @@ class Network:
         dpg.destroy_context()
 
 
+    def load_font(self):
+        """ 文字設定 """
+
+        with dpg.font_registry():
+            font_path = ""
+            if platform.system() == "Windows":
+                font_path = "C:/Windows/Fonts/msjh.ttc"  # 微軟正黑體
+            elif platform.system() == "Darwin":
+                font_path = "/System/Library/Fonts/PingFang.ttc" # Mac 蘋方體
+
+            if font_path and os.path.exists(font_path):
+                with dpg.font(font_path, 18) as default_font:   # 字體大小 : 18
+                    dpg.add_font_range_hint(dpg.mvFontRangeHint_Default)
+                    dpg.add_font_range_hint(dpg.mvFontRangeHint_Chinese_Full)
+
+                dpg.bind_font(default_font)
+
+
     def save_setting(self, *args):   # args 接收傳來的其他參數
         """儲存設定"""
 
@@ -87,88 +177,53 @@ class Network:
         self.output_message(f"[System] : IP saved -- {self.target_ip}:{self.target_port}")
         self.output_message(f"[System] : Data formate set to {self.data_format}")
 
-    def establish_connect(self):
-        """建立連線"""
-
-        try:
-            self.s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)  # AF_INET : IPv4 協定 ; SOCK_STREAM : TCP 協定
-            self.s.connect((self.target_ip, int(self.target_port)))
-            self.output_message("[System] : Connect sucessfully")
-            self.is_connected = True
-            dpg.set_item_label("btn_connect", "Disconnect")
-
-            # 用threading建立多執行緒
-            t = threading.Thread(target=self.receive_data, daemon=True) # daemon=True -> 主執行緒被關閉時，副執行緒也同樣會被關閉 
-            t.start()
-            
-        except Exception as e:
-            self.output_message(f"[System] : Connect failed -- {e}")
-            self.s.close()
-
-
-    def stop_connect(self):
-        """中斷連線"""
-
-        self.is_connected = False
-        if self.s:
-            try:
-                self.s.shutdown(socket.SHUT_RDWR)
-                self.s.close()
-            except:
-                pass
-        
-        dpg.set_item_label("btn_connect", "Connect")        
-        self.output_message("[System] : Disconnected")
-
 
     def toggle_connection(self):
         """切換連線按鈕"""
-        if not self.is_connected:
+
+        if not self.network.is_connected:
             self.output_message("[System] : Trying to connect...")
-            self.establish_connect()
+            connect_success, msg = self.network.establish_connect(self.target_ip, self.target_port)
+        
+            if connect_success:
+                dpg.set_item_label("btn_connect", "Disconnect")
+                self.output_message("[System] : Connect sucessfully")
+            else:
+                self.output_message(f"[System] : Connect failed -- {msg}")
+
         else:
-            self.stop_connect()
+            self.network.stop_connect()
+            dpg.set_item_label("btn_connect", "Connect")        
+            self.output_message("[System] : Disconnected")
 
 
-    def receive_data(self):
-        """接收資料"""
+    def handle_incoming(self, data):
+        """處理 NetworkClient 下 receive_data 接收的資料"""
 
-        while self.is_connected:
-            try:      
-                self.data = self.s.recv(1024)
-                if not self.data:
-                    self.output_message("[System] : Sever disconnect")
-                    break
-                
-                # 資料輸出型態
-                if self.data_format == "Text(UTF-8)":
-                    self.output_message(f">> {self.data.decode('utf-8', errors='replace')}")
-                elif self.data_format == "Hex":
-                    self.output_message(f">> {self.data.hex(' ').upper()}") # hex(' ') 的作用是在每個 Byte 之間插空格
-                elif self.data_format == "Binary":
-                    # for 跑 bytes (self.data) 會把每個 byte 變成 0~255 的整數
-                    # '08b' : b -> 二進位, 8 -> 讓字串是八個字元, 0 -> 如果有缺補零
-                    self.output_message(f">> {' '.join(format(byte, '08b') for byte in self.data)}")
-
-            except Exception as e:
-                if self.is_connected:
-                    self.output_message(f"[System] : Error -- {e}")
-                break
-
-        self.is_connected = False
-        dpg.set_item_label("btn_connect", "Connect")        
+        if data == None:    # 斷線
+            dpg.set_item_label("btn_connect", "Connect")        
+            self.output_message("[System] : Sever disconnect")
+            return
+        
+        elif isinstance(data, Exception):   # 錯誤訊息
+            self.output_message(f"[System] : Error -- {data}")
+            return
+        
+        else:
+            output_data = self.processor.format_output(data, self.data_format)
+            self.output_message(output_data)
 
 
     def output_message(self, message):
         """"滾動窗輸出訊息 & 整理頁面"""
 
         dpg.add_text(message, parent="log_window")
-        dpg.set_y_scroll("log_window", 999)
+        dpg.set_y_scroll("log_window", 999999)
 
         if len(dpg.get_item_children("log_window", 1)) > 500:
             dpg.delete_item(dpg.get_item_children("log_window", 1)[0]) # [0] -> 最上面的一行
-        
+
 
 # 執行視窗
 if __name__ == "__main__":
-    Network()
+    AppWindow()
